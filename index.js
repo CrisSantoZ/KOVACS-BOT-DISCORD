aabfdafad11116b91c7cc94fbf4fe5808607ef61
CrisSantoZ <ozzcris@hotmail.com>
2025-06-05 13:12:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -557,7 +557,7 @@ if (actionRow.components.length < 5 && (!temOpcoesParaBotoes || resultadoInterac
                     } else { 
                         await interaction.reply(payload); 
                     }
-                }```text
+                }
             } else if (!['criar', 'ficha', 'interagir'].includes(commandName)) { 
                 console.warn(`[RESPOSTA] 'respostaParaEnviar' é undefined para /${commandName}, e este comando não respondeu diretamente à interação.`);
             } 
