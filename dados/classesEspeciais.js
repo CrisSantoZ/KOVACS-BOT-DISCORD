3c127f15b90ba02de1949a9cdfcaf6da855a6c2d
CrisSantoZ <ozzcris@hotmail.com>
2025-06-07 13:58:49 -0300

    Create classesEspeciais.js

---
+++ dados/classesEspeciais.js
@@ -0,0 +1,21 @@
+//Lista de Classes Especiais
+module.exports = [
+  { nome: "Guerreiro Alquimista",
+        desc: "Usa alquimia para forjar armas como espada, lança, arco e flecha se tiver os materiais necessários, defesas entre outras coisas envolvendo Alquimia. (Jam)",
+        nomeCargo: "Classe Especial: Guerreiro Alquimista" },
+    { nome: "Ferreiro Divino",
+        desc: "Divindade do fogo, dos metais e da metalurgia, consegue manipular qualquer material desde que haja mana da natureza a sua volta, e criações de armas ou armaduras apenas se houver matérias para tal. (Drak)",
+        nomeCargo: "Classe Especial: Ferreiro Divino" },
+    { nome: "Arauto Da Fortaleza",
+        desc: "Um pilar de apoio no campo de batalha, capaz de alterar o curso de confrontos com curas e auxílios vitais, inspirado em lendas como Faramis. (Isac)",
+        nomeCargo: "Classe Especial: Arauto Da Fortaleza" },
+    { nome: "Arlequim Sanguinário", 
+        desc: "Um assassino caótico e imprevisível com um senso de humor distorcido, encontrando alegria na matança e na confusão. (Erick)",
+       nomeCargo: "Classe Especial: Arlequim Sanguinário" },
+    { nome: "Rei Esquecido",
+        desc: "Um monarca de outrora cujo poder ancestral permite uma transformação colossal, liberando uma força destrutiva comparável à de um Titã. (Felipe)",
+        nomeCargo: "Classe Especial: Rei Esquecido" },
+    { nome: "Deus Necromante",
+        desc: "Um ser que transcende os limites mortais, controlando os fluxos da vida e da morte com seus poderes necromânticos infundidos por uma energia roxa sinistra. (ADM Master)",
+        nomeCargo: "Classe Especial: Deus Necromante"}
+];
