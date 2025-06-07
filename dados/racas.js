5b24f5c2141792cc918ea8732fcfdb9edc0da5c2
CrisSantoZ <ozzcris@hotmail.com>
2025-06-07 13:46:37 -0300

    Create racas.js

---
+++ dados/racas.js
@@ -0,0 +1,6 @@
+// Lista de raças
+module.exports = [
+    { id: "humano", nome: "Humano", descricao: "Versátil e adaptável." },
+    { id: "elfo", nome: "Elfo", descricao: "Ágil e ligado à natureza." },
+    // ...adicione suas raças
+];
