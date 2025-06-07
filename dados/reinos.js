336d5a07999f902e50d9b69995546a4260716d52
CrisSantoZ <ozzcris@hotmail.com>
2025-06-07 14:00:02 -0300

    Create reinos.js

---
+++ dados/reinos.js
@@ -0,0 +1,10 @@
+//Lista de Reinos
+module.exports = [
+  { nome: "Valdoria", desc: "Reino central dos humanos, conhecido por sua diplomacia, comércio e exércitos bem treinados. Valoriza a ordem e a justiça.", nomeCargo: "Reino: Valdoria" },
+    { nome: "Elarion", desc: "Antiga e mística floresta élfica, guardiã de segredos ancestrais e magia da natureza. Os Eldari e Mei'ra frequentemente chamam este lugar de lar.", nomeCargo: "Reino: Elarion" },
+    { nome: "Durnholde", desc: "Reino anão escavado nas profundezas das montanhas, famoso por suas minas ricas, metalurgia incomparável e resistência inabalável.", nomeCargo: "Reino: Durnholde" },
+    { nome: "Caelum", desc: "Cidade flutuante dos Seraphim, um bastião de luz e conhecimento arcano, raramente visitado por outras raças.", nomeCargo: "Reino: Caelum" },
+    { nome: "Ravengard", desc: "Domínio desolado e sombrio onde os Sombrios e outras criaturas da noite encontram refúgio. Um lugar de perigo constante e magia profana.", nomeCargo: "Reino: Ravengard" },
+    { nome: "Thornmere", desc: "Vasto território livre, composto por planícies, pântanos e pequenas vilas. É uma terra de fronteira, habitada por diversas raças e facções.", nomeCargo: "Reino: Thornmere" },
+    { nome: "Isle of Morwyn", desc: "Ilha envolta em névoas e magia proibida, lar de segredos arcanos perigosos e relíquias de poder imenso. Poucos ousam se aventurar por suas costas.", nomeCargo: "Reino: Isle of Morwyn" }
+];
